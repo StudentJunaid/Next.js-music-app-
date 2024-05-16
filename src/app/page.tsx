5fca45c722e959695c31ee46fe80b87e7d9a1c44
Junaid Noor <junaidkhan97991@gmail.com>
2024-05-16 03:52:57 -0700

    TestimonialCards &  featuredSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import HeroSection from "../components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooesus from "@/components/WhyChooesus";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
 
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.0]">
     <HeroSection/>
+    <FeaturedCourses/>
+    <WhyChooesus/>
+    <MusicSchoolTestimonials/>
   </main>
   );
 }
